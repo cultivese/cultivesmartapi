79f855a08ed12b0987b5a22bf2d4ba35fd405fe2
unknown <administrador>
2024-12-24 07:23:05 -0300

    feat: cors

--- index.php
+++ index.php
@@ -5,6 +5,10 @@ header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS"); // Mét
 header("Access-Control-Allow-Headers: Content-Type, Authorization"); // Cabeçalhos permitidos
 header("Access-Control-Allow-Credentials: true"); // Se autenticação com cookies for usada
 
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200); // Responder a requisições preflight
+    exit();
+}
 
 require './api/fornecedor.php';
 require 'db/connection.php';
